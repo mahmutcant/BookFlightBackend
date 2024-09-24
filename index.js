d1a3c324308f6ad5a3811400f226809441eaea59
mahmutcant <mahcan012@gmail.com>
2024-09-24 19:27:30 +0300

    update gitignore

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const connectDB = require('./db');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
